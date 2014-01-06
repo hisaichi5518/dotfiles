77f2f064036f142d98e55304576dafdcc3326bea
hisaichi5518 <hisaichi5518@gmail.com>
2014-01-06 14:59:36 +0900

    alias追加した

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,5 +1,8 @@
 alias tmux="tmux -f ~/.tmux.conf"
 alias gs="git status"
 alias gl="tig"
+alias lg=gl
 alias vi='/Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-alias l="ls"
+alias iv=vi
+alias v=vi
+alias l=ls
