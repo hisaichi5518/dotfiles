69272d0342ed01231692e66dca1d3b303b16a172
hisaichi5518 <hisaichi5518@gmail.com>
2014-06-17 10:34:51 +0900

    リファクタリングした

--- .zsh/peco-git-checkout.zsh
+++ .zsh/peco-git-checkout.zsh
@@ -1,7 +1,9 @@
 function peco-git-checkout () {
-    local selected_branch=$(git branch | sed 's/^\*//' | sed 's/\ //g' | peco --query "$LBUFFER")
-    BUFFER="git checkout ${selected_branch}"
-    zle accept-line
+    local selected_branch=$(git branch | sed 's/[\* ]//g' | peco --query "$LBUFFER")
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git checkout ${selected_branch}"
+        zle accept-line
+    fi
     zle clear-screen
 }
 zle -N peco-git-checkout
