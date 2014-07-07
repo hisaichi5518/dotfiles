c014fa2938a478022e179947c10b676edbba1eb1
hisaichi5518 <hisaichi5518@gmail.com>
2014-07-07 11:39:10 +0900

    一つのファイルにまとめた

---
+++ .zsh/peco-functions.zsh
@@ -0,0 +1,39 @@
+function peco-function-list () {
+    local selected=$(functions | grep "^.*\ ()\ {" | sed -e "s| () {||" | grep peco- | grep -v function-list | peco --query "$LBUFFER")
+    if [ -n "$selected" ]; then
+        ${selected}
+    fi
+}
+zle -N peco-function-list
+bindkey '^p' peco-function-list
+
+function peco-ghq () {
+    local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-ghq
+bindkey '^s' peco-ghq
+
+function peco-git-checkout () {
+    local selected_branch=$(git branch | sed 's/[\* ]//g' | peco --query "$LBUFFER")
+    if [ -n "$selected_branch" ]; then
+        BUFFER="git checkout ${selected_branch}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-git-checkout
+bindkey '^b' peco-git-checkout
+
+function peco-select-history() {
+    # oh-my-zshがhistoryを上書きしている
+    BUFFER=$(\history -n 0 | tail -r | peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
--- .zsh/peco-select-history.zsh
+++
@@ -1,8 +0,0 @@
-function peco-select-history() {
-    # oh-my-zshがhistoryを上書きしている
-    BUFFER=$(\history -n 0 | tail -r | peco --query "$LBUFFER")
-    CURSOR=$#BUFFER
-    zle clear-screen
-}
-zle -N peco-select-history
-bindkey '^r' peco-select-history
